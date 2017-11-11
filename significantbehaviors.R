b02540895f405d9275e3ae212c01c14fc5988367
JosefineHolm <jojoholm@hotmail.com>
2017-11-11 15:49:26 -0800

    The plots for the residuals do not wanna run after I change the data set to females instead of care.

--- significantbehaviors.R
+++ significantbehaviors.R
@@ -1,23 +1,30 @@
+
+###########################Statistics run with significant results############################## 
+
 #load data
 care=read.table(file="caredata.csv", header=TRUE, sep=",")
 names(care)
 str(care)
 
-#Packages used
+#Installed Packages
+install.packages("lme4")
+install.packages("geepack")
+install.packages("lsmeans")
+install.packages("dplyr")
+install.packages("MuMIn")
+install.packages("ggplot2")
+install.packages("nortest")
+
+#Library to run 
 library(nlme)
 library(dplyr)
 #packages gotten from last years project
 library(lattice)
-install.packages("lme4")
 library(lme4)
 library(geepack)
-install.packages("lsmeans")
 library(lsmeans)
-install.packages("dplyr")
 library(dplyr)
-install.packages("MuMIn")
 library(MuMIn)
-install.packages("ggplot2")
 library(ggplot2)
 
 #Putting the 4 condition in as a factor
@@ -28,17 +35,18 @@ str(care)
 xx=care[,sapply(care,is.integer)] #this is to make a subset only of the integers (numbers)
 xx
 
-
-###Present to baboons
-#model one without random factor
-m1=gls(pbaboon~cond.f, data=care, na.action=na.omit, method="ML")
+##########################################Gouped behaviors#####################################
+#Neutral duration behaviors
+m1=gls(neutrald~cond.f, data=care, na.action=na.omit, method="ML")
 summary(m1)
 
 #model2 - try put individual in as a main factor so cond.f+id
-m2=lme(pbaboon~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+m2=lme(neutrald~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
 summary(m2)
 anova(m1,m2)
-#Since the model with the random factor had a lower IAC score we wanna use that 
+#Since the model with the random factor had a lower IAC score we wanna use that. Model 2 is also significant P=0.0019
+# It is also significant P = 0,0354
+
 #Plotting residuals for m2 to check if we can use this model
 op=par(mfrow=c(2,2), mar=c(5,4,1,2))
 plot(m2, add.smooth=FALSE, which=1)
@@ -49,28 +57,28 @@ plot(care$id, E, xlab="id", ylab="residuals")
 
 qqnorm(residuals(m2))
 qqline(residuals(m2))
-ad.test(residuals(m2))#this one says error
+ad.test(residuals(m2))#This ad.test is also significant P=2.2*10^-16
 summary(m2)
 
 lsmeans(m2,pairwise~cond.f)#This one schould be able to show how the behaviors have changes
-#There are a significant change between condition 1 and 2, 1 and 3 and 1 and 4. 
+#There are a significant difference between condition 3 and 4 P = 0.0191, and almost between condition 1 and 4 P=0,0703.  
 
 #making a table that will show the means per individual that can be graphed not sure it works
 x1 <- group_by(care, cond.f, id) %>%
-  summarize(m.pbaboon = mean(pbaboon, na.rm = TRUE), # na.rm = TRUE to remove missing values
-            s.pbaboon=sd(pbaboon, na.rm = TRUE),  # na.rm = TRUE to remove missing values
-            n = sum(!is.na(pbaboon)), # of observations, excluding NAs.
-            se.pbaboon=s.pbaboon/sqrt(n))
+  summarize(m.neutrald = mean(neutrald, na.rm = TRUE), # na.rm = TRUE to remove missing values
+            s.neutrald=sd(neutrald, na.rm = TRUE),  # na.rm = TRUE to remove missing values
+            n = sum(!is.na(neutrald)), # of observations, excluding NAs.
+            se.neutrald=s.neutrald/sqrt(n))
 x1
 #Trying to plot the amount of neutral counts there are for each individual
 ggplot(data=x1,
-       aes(x=cond.f, y=m.pbaboon, fill=id, label=m.pbaboon)) +
+       aes(x=cond.f, y=m.neutrald, fill=id, label=m.neutrald)) +
   geom_bar(stat="identity", position=position_dodge(), color = "black") +
-  geom_errorbar(aes(ymin=m.pbaboon, ymax=m.pbaboon+se.pbaboon), width=0.2,
+  geom_errorbar(aes(ymin=m.neutrald, ymax=m.neutrald+se.neutrald), width=0.2,
                 position=position_dodge(0.9)) +
   scale_fill_manual(values=c("black","white", "light grey", "dark grey")) +
   xlab("ID") +
-  ylab("Number of presentations to baboons") +
+  ylab("Duration of neutral behaviors") +
   ylim(0,15) +
   labs(fill="id") +
   theme_bw() +
@@ -86,17 +94,19 @@ ggplot(data=x1,
         axis.title.y=element_text(size=8),
         axis.title.x=element_text(size=8),
         axis.text.x=element_text(size=8))
- 
-#### lipsmack
-#model one without random factor
-m1=gls(lipsmack~cond.f, data=care, na.action=na.omit, method="ML")
+#The graph does not work
+
+#Negative  count behaviors
+m1=gls(negativect~cond.f, data=care, na.action=na.omit, method="ML")
 summary(m1)
 
 #model2 - try put individual in as a main factor so cond.f+id
-m2=lme(lipsmack~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+m2=lme(negativect~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
 summary(m2)
 anova(m1,m2)
-#Since the model with the random factor had a lower IAC score we wanna use that 
+#Since the model with the random factor had a lower IAC score we wanna use that. 
+# It is also significant P = 0,0141
+
 #Plotting residuals for m2 to check if we can use this model
 op=par(mfrow=c(2,2), mar=c(5,4,1,2))
 plot(m2, add.smooth=FALSE, which=1)
@@ -107,11 +117,70 @@ plot(care$id, E, xlab="id", ylab="residuals")
 
 qqnorm(residuals(m2))
 qqline(residuals(m2))
-ad.test(residuals(m2))#this one says error
+ad.test(residuals(m2))#This ad.test is also significant P=2.2*10^-16
 summary(m2)
 
 lsmeans(m2,pairwise~cond.f)#This one schould be able to show how the behaviors have changes
-#There are a significant change between condition 1 and 2. 
+#There are significant difference between 1-2 P=0,0058, 1-3 P=0.0002, 1-4 P=0.0017.  
+
+#making a table that will show the means per individual that can be graphed not sure it works
+x1 <- group_by(care, cond.f, id) %>%
+  summarize(m.negativect = mean(negativect, na.rm = TRUE), # na.rm = TRUE to remove missing values
+            s.negativect=sd(negativect, na.rm = TRUE),  # na.rm = TRUE to remove missing values
+            n = sum(!is.na(negativect)), # of observations, excluding NAs.
+            se.negativect=s.negativect/sqrt(n))
+x1
+#Trying to plot the amount of neutral counts there are for each individual
+ggplot(data=x1,
+       aes(x=cond.f, y=m.negativect, fill=id, label=m.negativect)) +
+  geom_bar(stat="identity", position=position_dodge(), color = "black") +
+  geom_errorbar(aes(ymin=m.negativect, ymax=m.negativect+se.negativect), width=0.2,
+                position=position_dodge(0.9)) +
+  scale_fill_manual(values=c("black","white", "light grey", "dark grey")) +
+  xlab("ID") +
+  ylab("Counts of negative behaviors") +
+  ylim(0,15) +
+  labs(fill="id") +
+  theme_bw() +
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        legend.title=element_text(size=6),
+        legend.key=element_blank(),
+        legend.position=c(0.5,0.95),
+        legend.text=element_text(size=8),
+        legend.background=element_blank(),
+        legend.direction="horizontal",
+        legend.key.size=unit(0.3, "cm"),
+        axis.title.y=element_text(size=8),
+        axis.title.x=element_text(size=8),
+        axis.text.x=element_text(size=8))
+
+######################################Individual behaviors#########################################
+#Lipsmacks
+#model one without random factor
+m1=gls(lipsmack~cond.f, data=care, na.action=na.omit, method="ML")
+summary(m1)
+
+#model2 - try put individual in as a main factor so cond.f+id
+m2=lme(lipsmack~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+summary(m2)
+anova(m1,m2)
+#Since the model with the random factor had a lower IAC score we wanna use that and its significant P=0.0001
+#Plotting residuals for m2 to check if we can use this model
+op=par(mfrow=c(2,2), mar=c(5,4,1,2))
+plot(m2, add.smooth=FALSE, which=1)
+E=resid(m2)
+hist(E,xlab="residuals", main="")
+plot(care$cond.f, E, xlab="Treatment", ylab="residuals")
+plot(care$id, E, xlab="id", ylab="residuals")
+
+qqnorm(residuals(m2))
+qqline(residuals(m2))
+ad.test(residuals(m2))#ad.test is significant P=2.2*10^-16
+summary(m2)
+
+lsmeans(m2,pairwise~cond.f)#This one schould be able to show how the behaviors have changes
+#There are a significant change between condition 1 and 2. P=0.0398
 
 #making a table that will show the means per individual that can be graphed not sure it works
 x1 <- group_by(care, cond.f, id) %>%
@@ -145,7 +214,7 @@ ggplot(data=x1,
         axis.title.x=element_text(size=8),
         axis.text.x=element_text(size=8))
 
-#####Present to baboons
+###Present to baboons
 #model one without random factor
 m1=gls(pbaboon~cond.f, data=care, na.action=na.omit, method="ML")
 summary(m1)
@@ -154,7 +223,7 @@ summary(m1)
 m2=lme(pbaboon~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
 summary(m2)
 anova(m1,m2)
-#Since the model with the random factor had a lower IAC score we wanna use that 
+#Since the model with the random factor had a lower IAC score we wanna use that and its significant P=8*10^-4 
 #Plotting residuals for m2 to check if we can use this model
 op=par(mfrow=c(2,2), mar=c(5,4,1,2))
 plot(m2, add.smooth=FALSE, which=1)
@@ -165,11 +234,11 @@ plot(care$id, E, xlab="id", ylab="residuals")
 
 qqnorm(residuals(m2))
 qqline(residuals(m2))
-ad.test(residuals(m2))#this one says error
+ad.test(residuals(m2))#ad.test is significant P=2.2*10^-16 
 summary(m2)
 
 lsmeans(m2,pairwise~cond.f)#This one schould be able to show how the behaviors have changes
-#There are a significant change between condition 1 and 2, 1 and 3 and 1 and 4. 
+#There are a significant change between condition 1 and 2 P=0.0042, 1 and 3 P=0.0022and 1 and 4 P=0.0026. 
 
 #making a table that will show the means per individual that can be graphed not sure it works
 x1 <- group_by(care, cond.f, id) %>%
@@ -203,7 +272,7 @@ ggplot(data=x1,
         axis.title.x=element_text(size=8),
         axis.text.x=element_text(size=8))
 
-####### Present to human 
+#### Present to human 
 m1=gls(phuman~cond.f, data=care, na.action=na.omit, method="ML")
 summary(m1)
 
@@ -211,7 +280,7 @@ summary(m1)
 m2=lme(phuman~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
 summary(m2)
 anova(m1,m2)
-#Since the model with the random factor had a lower IAC score we wanna use that 
+#Since the model with the random factor had a lower IAC score we wanna use that and its significant P=0.001
 #Plotting residuals for m2 to check if we can use this model
 op=par(mfrow=c(2,2), mar=c(5,4,1,2))
 plot(m2, add.smooth=FALSE, which=1)
@@ -222,11 +291,11 @@ plot(care$id, E, xlab="id", ylab="residuals")
 
 qqnorm(residuals(m2))
 qqline(residuals(m2))
-ad.test(residuals(m2))#this one says error
+ad.test(residuals(m2))#ad.test is significant P=2.2*10^-16
 summary(m2)
 
 lsmeans(m2,pairwise~cond.f)#This one schould be able to show how the behaviors have changes
-#There are a significant change between condition 1 and 4 and 2 and 4.
+#There are a significant change between condition 2 and 4 P=0.0458 and almost 1-4.
 
 #making a table that will show the means per individual that can be graphed not sure it works
 x1 <- group_by(care, cond.f, id) %>%
@@ -259,6 +328,7 @@ ggplot(data=x1,
         axis.title.y=element_text(size=8),
         axis.title.x=element_text(size=8),
         axis.text.x=element_text(size=8))
+
 ##### Embrace baboon
 m1=gls(embraceb~cond.f, data=care, na.action=na.omit, method="ML")
 summary(m1)
@@ -267,7 +337,7 @@ summary(m1)
 m2=lme(embraceb~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
 summary(m2)
 anova(m1,m2)
-#Since the model with the random factor had a lower IAC score we wanna use that 
+#Since the model with the random factor had a lower IAC score we wanna use that but its not significant p=0.1007 
 #Plotting residuals for m2 to check if we can use this model
 op=par(mfrow=c(2,2), mar=c(5,4,1,2))
 plot(m2, add.smooth=FALSE, which=1)
@@ -278,11 +348,11 @@ plot(care$id, E, xlab="id", ylab="residuals")
 
 qqnorm(residuals(m2))
 qqline(residuals(m2))
-ad.test(residuals(m2))#this one says error
+ad.test(residuals(m2))#ad.test is significant 2.2*10^-16
 summary(m2)
 
 lsmeans(m2,pairwise~cond.f)#This one schould be able to show how the behaviors have changes
-#There are a significant difference between 1-3 and almost 2-3
+#There are a significant difference between 1-3 P = 0.0024 and almost 2-3 P=0.0527.
 
 #making a table that will show the means per individual that can be graphed not sure it works
 x1 <- group_by(care, cond.f, id) %>%
@@ -315,3 +385,4 @@ ggplot(data=x1,
         axis.title.y=element_text(size=8),
         axis.title.x=element_text(size=8),
         axis.text.x=element_text(size=8))
+

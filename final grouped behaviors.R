8786aed14ea11d8827f091fb829e1b49ee6c5944
JosefineHolm <jojoholm@hotmail.com>
2018-02-12 14:51:26 -0800

    recieve

--- final grouped behaviors.R
+++ final grouped behaviors.R
@@ -51,11 +51,8 @@ care$neutrald
 #Positive count behaviors
 #model one without random factor
 m1=gls(positivect~cond.f, data=care, na.action=na.omit, method="ML")
-summary(m1)
 #model2 - try put individual in as a main factor so cond.f+id
 m2=lme(positivect~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
-summary(m2)
-lsmeans(m2,pairwise~cond.f)
 #Normalizing
 care$l.positivect=log(care$positivect+1)
 
@@ -140,4 +137,210 @@ ggplot(data=x1,
         axis.title.x=element_text(size=8),
         axis.text.x=element_text(size=8))
 #####################################################################################
+#Positive duration behaviors
+#model one without random factor
+m1=gls(positived~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2=lme(positived~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+#Normalizing
+care$l.positived=log(care$positived+1)
+#Now we run same models with the normalised data
+m1n=gls(l.positived~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2n=lme(l.positived~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+anova(m1n,m2n)
+#Test variance equality if the p is sign run alternate variance structure, I even tried an the normal model is still better. 
+bartlett.test(l.positived~id, data=care)
+#P is not significant so we use the normalised m2e
+#Now checking for significant results
+model.matrix.gls <- function(m2n, ...){
+  
+  model.matrix(terms(m2n), data = getData(m2n), ...) 
+  
+}
 
+model.frame.gls <- function(m2n, ...){
+  
+  model.frame(formula(m2n), data = getData(m2n), ...) 
+  
+}
+
+terms.gls <- function(m2n, ...){
+  
+  terms(model.frame(m2n),...) 
+  
+}
+
+
+
+multCompTukey <- glht(m2n, linfct = mcp(cond.f = "Tukey"))
+
+summary(multCompTukey)
+#There are no significant differences. 
+##################################################################################
+#Negative count behaviors
+#model one without random factor
+m1=gls(negativect~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2=lme(negativect~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+#Normalizing
+care$l.negativect=log(care$negativect+1)
+
+#Now we run same models with the normalised data
+m1n=gls(l.negativect~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2n=lme(l.negativect~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+anova(m1n,m2n)
+#Test variance equality if the p is sign run alternate variance structure
+bartlett.test(l.negativect~id, data=care)
+#P is not significant so we use the noamlised m2n model 
+vf1=varIdent(form = ~1|id)
+vf2=varIdent(form = ~1|cond.f)
+m2n=lme(l.negativect~cond.f, random=~1|id,data=care, na.action=na.omit)
+
+M2.1<-lme(l.negativect~cond.f, random=~1|id,data=care, na.action=na.omit, weights=vf1)
+
+M2.2<-lme(l.negativect~cond.f, random=~1|id,data=care, na.action=na.omit, weights=vf2)
+anova(m2n,M2.1,M2.2)
+#M2.1 is the better model 
+#Now checking for significant results
+model.matrix.gls <- function(m2n, ...){
+  
+  model.matrix(terms(m2n), data = getData(m2n), ...) 
+  
+}
+
+model.frame.gls <- function(m2n, ...){
+  
+  model.frame(formula(m2n), data = getData(m2n), ...) 
+  
+}
+
+terms.gls <- function(m2n, ...){
+  
+  terms(model.frame(m2n),...) 
+  
+}
+
+
+
+multCompTukey <- glht(m2n, linfct = mcp(cond.f = "Tukey"))
+
+summary(multCompTukey)
+#There are significant difference between cond 1-2, 1-3 and 1-4. There are more negative counts in cond 1 than 2,3,4. 
+#Looking at residuals
+op=par(mfrow=c(2,2), mar=c(5,4,1,2))
+plot(M2.2, add.smooth=FALSE, which=1)
+E=resid(M2.2)
+hist(E,xlab="residuals", main="")
+plot(filter(care, !is.na(l.negativect)) %>% dplyr::select(id), #this code will filter NAs out.  it's best to use this nested in the plot command all the time
+     E, xlab="Treatment", ylab="residuals")
+plot(filter(care, !is.na(l.negativect)) %>% dplyr::select(cond.f), #this code will filter NAs out.  it's best to use this nested in the plot command all the time
+     E, xlab="Treatment", ylab="residuals")
+
+qqnorm(residuals(M2.2))
+qqline(residuals(M2.2))
+ad.test(residuals(M2.2))
+summary(M2.2)
+##################################################################################
+#Neutral count behaviors
+#model one without random factor
+m1=gls(neutralct~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2=lme(neutralct~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+#Normalizing
+care$l.neutralct=log(care$neutralct+1)
+
+#Now we run same models with the normalised data
+m1n=gls(l.neutralct~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2n=lme(l.neutralct~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+anova(m1n,m2n)
+#Test variance equality if the p is sign run alternate variance structure
+bartlett.test(l.neutralct~id, data=care)
+#P is significant so we use alternate variance models 
+vf1=varIdent(form = ~1|id)
+vf2=varIdent(form = ~1|cond.f)
+m2n=lme(l.neutralct~cond.f, random=~1|id,data=care, na.action=na.omit)
+
+M2.1<-lme(l.neutralct~cond.f, random=~1|id,data=care, na.action=na.omit, weights=vf1)
+
+M2.2<-lme(l.neutralct~cond.f, random=~1|id,data=care, na.action=na.omit, weights=vf2)
+anova(m2n,M2.1,M2.2)
+#M2.1 is the better model 
+#Now checking for significant results
+model.matrix.gls <- function(M2.1, ...){
+  
+  model.matrix(terms(M2.1), data = getData(M2.1), ...) 
+  
+}
+
+model.frame.gls <- function(M2.1, ...){
+  
+  model.frame(formula(M2.1), data = getData(M2.1), ...) 
+  
+}
+
+terms.gls <- function(M2.1, ...){
+  
+  terms(model.frame(M2.1),...) 
+  
+}
+
+
+
+multCompTukey <- glht(M2.1, linfct = mcp(cond.f = "Tukey"))
+
+summary(multCompTukey)
+#There are no significant results
+##########################################################################
+#Neutral duration behaviors
+#model one without random factor
+m1=gls(neutrald~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2=lme(neutrald~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+#Normalizing
+care$l.neutrald=log(care$neutrald+1)
+
+#Now we run same models with the normalised data
+m1n=gls(l.neutrald~cond.f, data=care, na.action=na.omit, method="ML")
+#model2 - try put individual in as a main factor so cond.f+id
+m2n=lme(l.neutrald~cond.f, random=~1|id,data=care, na.action=na.omit, method="ML")
+anova(m1n,m2n)
+#Test variance equality if the p is sign run alternate variance structure
+bartlett.test(l.neutrald~id, data=care)
+#P is significant so we use alternate variance models 
+vf1=varIdent(form = ~1|id)
+vf2=varIdent(form = ~1|cond.f)
+m2n=lme(l.neutrald~cond.f, random=~1|id,data=care, na.action=na.omit)
+
+M2.1<-lme(l.neutrald~cond.f, random=~1|id,data=care, na.action=na.omit, weights=vf1)
+
+M2.2<-lme(l.neutrald~cond.f, random=~1|id,data=care, na.action=na.omit, weights=vf2)
+anova(m2n,M2.1,M2.2)
+#M2.1 is the better model 
+#Now checking for significant results
+model.matrix.gls <- function(M2.1, ...){
+  
+  model.matrix(terms(M2.1), data = getData(M2.1), ...) 
+  
+}
+
+model.frame.gls <- function(M2.1, ...){
+  
+  model.frame(formula(M2.1), data = getData(M2.1), ...) 
+  
+}
+
+terms.gls <- function(M2.1, ...){
+  
+  terms(model.frame(M2.1),...) 
+  
+}
+
+
+
+multCompTukey <- glht(M2.1, linfct = mcp(cond.f = "Tukey"))
+
+summary(multCompTukey)
+#There are a significant difference between condition 1-4 and 3-4 and almost 2-4. Less neutral duration in cond 4 than 1 and 3. 
